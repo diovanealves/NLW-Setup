7609023539b4dcf362c0c83fd484bbd669a1f404
Diovane <alvdiovane@gmail.com>
2023-01-21 19:08:01 -0300

    Finish Aula 01,02,03,04

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = fastify();
+
+app.register(cors);
+app.register(appRoutes);
+
+app.listen({ port: 3000 }).then(() => {
+  console.log("Listening on port 3000");
+});
